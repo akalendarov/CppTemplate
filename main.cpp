4d9edea4d2ae9c9c7779f07bed04c5ca74985a38
Andrey E. Kalendarov <andreykalendarov@gmail.com>
2016-02-10 16:23:28 +0300

    Version 1.0

---
+++ main.cpp
@@ -0,0 +1,103 @@
+/*
+ * C++11 code templates for contests.
+ * @author: Andrey Kalendarov
+ * @e-mail: andreykalendarov@gmail.com
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+
+/* ________ CODE ________ */
+
+class Solution
+{
+public:
+	array<int, 10000000> a;
+
+	inline void solve()
+	{
+
+	}
+
+
+/* ______ TEMPLATE ______ */
+	Solution()
+	{
+		//cout.precision(20);
+		cin.tie(nullptr);
+#ifdef ANDREIKKAA
+		assert(freopen("input.txt", "r", stdin));
+		//assert(freopen("output.txt", "w", stdout));
+#else
+		//assert(freopen("input.txt", "r", stdin));
+		//assert(freopen("output.txt", "w", stdout));
+#endif
+	}
+
+	~Solution()
+	{
+		del("---\nTime: ", clock() / (ld) CLOCKS_PER_SEC);
+		cout.flush();
+		fclose(stdin);
+		fclose(stdout);
+	}
+
+private:
+	template<typename T>
+	inline void in(T &t)
+	{
+		cin >> t;
+	}
+
+	template<typename T, typename... Args>
+	inline void in(T &t, Args &... args)
+	{
+		cin >> t;
+		in(args...);
+	}
+
+	template<typename T>
+	inline void oul(T t)
+	{
+		cout << t;
+	}
+
+	template<typename T, typename... Args>
+	inline void oul(T x, Args... args)
+	{
+		cout << x;
+		oul(args...);
+	};
+
+	template<typename... Args>
+	inline void out(Args... args)
+	{
+		oul(args...);
+		cout << "\n";
+	};
+
+	template<typename... Args>
+	inline void del(Args... args)
+	{
+#ifdef ANDREIKKAA
+		out(args...);
+#endif
+	};
+
+	template<typename... Args>
+	inline void deb(Args... args)
+	{
+#ifdef ANDREIKKAA
+		out(args...);
+#endif
+	};
+};
+
+int main()
+{
+	static Solution s;
+	s.solve();
+}
---
+++ stress.py
@@ -0,0 +1,62 @@
+from random import randint, randrange, choice, shuffle
+from sys import stderr
+from time import time
+from subprocess import call
+
+TIME_LIMIT = 2
+TASK_NAME = "__Template_cpp__"
+INPUT_FILE = "input.txt"
+OUTPUT_FILE = "output.txt"
+
+
+def gen():
+    f = open(INPUT_FILE, "w")
+
+    f.close()
+
+
+def run():
+    start_time = time()
+    print("Started...")
+
+    return_code = call("./" + TASK_NAME)
+
+    if return_code != 0:
+        print("Runtime Error, return code", return_code, file=stderr)
+        exit(0)
+
+    finish_time = time()
+    print("Finished")
+
+    execution_time = finish_time - start_time
+
+    if execution_time > TIME_LIMIT:
+        print("Time Limit, execution time", execution_time, file=stderr)
+        exit(0)
+
+    print("Accepted")
+
+
+def solve():
+    f = open(INPUT_FILE)
+    g = open("correct.txt", "w")
+
+    f.close()
+    g.close()
+
+
+def check():
+    if call(["diff", OUTPUT_FILE, "correct.txt"]) != 0:
+        print("Wrong Answer", file=stderr)
+        exit(0)
+
+
+def main():
+    while True:
+        # gen()
+        run()
+        # solve()
+        # check()
+
+
+main()
