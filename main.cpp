3681afc4f4fbff2c85ba1c1c8b92ac7d6a9b72fd
Andrey E. Kalendarov <andreykalendarov@gmail.com>
2016-02-19 02:54:45 +0300

    Python-formatted output. Reader and writer class decomposition.

--- main.cpp
+++ main.cpp
@@ -4,97 +4,126 @@
  * @e-mail: andreykalendarov@gmail.com
  */
 
-#include <bits/stdc++.h>
+/* ______ TEMPLATE ______ */
 
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 typedef long double ld;
+
+class Reader
+{
+private:
+	void setup()
+	{ cin.tie(nullptr), ios_base::sync_with_stdio(false); }
+
+public:
+	Reader()
+	{ setup(); }
+
+	Reader(const char *s)
+	{ setup(), freopen(s, "r", stdin); }
+
+	~Reader()
+	{ fclose(stdin); }
+
+	template<typename T>
+	inline void operator()(T &t)
+	{ cin >> t; }
+
+	template<typename T, typename... Args>
+	inline void operator()(T &t, Args &... args)
+	{ operator()(t), operator()(args...); }
+};
+
+class Writer
+{
+private:
+	void setup()
+	{ /*cout.precision(20);*/ }
+
+public:
+	Writer()
+	{ setup(); }
+
+	Writer(const char *s)
+	{ setup(), freopen(s, "w", stdout); }
+
+	~Writer()
+	{ fflush(stdout), fclose(stdin); }
+
+	template<typename T>
+	inline void raw(T x)
+	{ cout << x; }
+
+	template<typename T, typename... Args>
+	inline void raw(T x, Args... args)
+	{ raw(x), raw(args...); }
+
+	inline void operator()()
+	{ raw('\n'); }
+
+	template<typename T>
+	inline void operator()(T t)
+	{ raw(t), raw('\n'); }
+
+	template<typename T, typename... Args>
+	inline void operator()(T x, Args... args)
+	{ raw(x), raw(' '), operator()(args...); };
+};
+
+class Debug
+{
+public:
+	Debug()
+	{ }
+
+	~Debug()
+	{ operator()("Time:", clock() / (ld) CLOCKS_PER_SEC); }
+
+	template<typename T>
+	inline void raw(T x)
+	{
+#ifdef ANDREIKKAA
+		cout << x;
+#endif
+	}
+
+	template<typename T>
+	inline void fmt(T t)
+	{ raw(t); }
+
+	template<typename T, typename... Args>
+	inline void fmt(T x, Args... args)
+	{ raw(x), raw(' '), fmt(args...); };
+
+	template<typename... Args>
+	inline void operator()(Args... args)
+	{ raw("<<"), fmt(args...), raw(">>"), raw('\n'); };
+};
 
 /* ________ CODE ________ */
 
 class Solution
 {
 public:
-
+	Reader in;
+	Writer out;
+	Debug deb;
 
 	inline void solve()
 	{
 
 	}
 
+	Solution() :
 
-/* ______ TEMPLATE ______ */
-	Solution()
-	{
-		//cout.precision(20);
-		cin.tie(nullptr);
-		ios_base::sync_with_stdio(false);
 #ifdef ANDREIKKAA
-		assert(freopen("input.txt", "r", stdin));
-		//assert(freopen("output.txt", "w", stdout));
+			in("input.txt"), out(), deb()
 #else
-		//assert(freopen("input.txt", "r", stdin));
-		//assert(freopen("output.txt", "w", stdout));
+	in(), out(), deb()
 #endif
-	}
-
-	~Solution()
-	{
-		del("---\nTime: ", clock() / (ld) CLOCKS_PER_SEC);
-		cout.flush();
-		fclose(stdin);
-		fclose(stdout);
-	}
-
-private:
-	template<typename T>
-	inline void in(T &t)
-	{
-		cin >> t;
-	}
-
-	template<typename T, typename... Args>
-	inline void in(T &t, Args &... args)
-	{
-		cin >> t;
-		in(args...);
-	}
-
-	template<typename T>
-	inline void oul(T t)
-	{
-		cout << t;
-	}
-
-	template<typename T, typename... Args>
-	inline void oul(T x, Args... args)
-	{
-		cout << x;
-		oul(args...);
-	};
-
-	template<typename... Args>
-	inline void out(Args... args)
-	{
-		oul(args...);
-		cout << "\n";
-	};
-
-	template<typename... Args>
-	inline void del(Args... args)
-	{
-#ifdef ANDREIKKAA
-		out(args...);
-#endif
-	};
-
-	template<typename... Args>
-	inline void deb(Args... args)
-	{
-#ifdef ANDREIKKAA
-		out(args...);
-#endif
-	};
+	{ }
 };
 
 int main()
