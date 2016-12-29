65b2fe9198d5bdd6362609232b8483aff13928a1
Andrei Kalendarov <andrey.kalendarov@gmail.com>
2016-12-29 18:51:05 +0300

    bits/stdc++.h added.

--- main.cpp
+++ main.cpp
@@ -4,10 +4,6 @@
 * @e-mail: andreykalendarov@gmail.com
 */
 
-/*______ DEFINES _______*/
-
-#define _SCL_SECURE_NO_WARNINGS
-#define _CRT_SECURE_NO_WARNINGS
 //#define ANDREIKKAA_TOPCODER
 //#define ANDREIKKAA_ALLOCATOR
 #define ANDREIKKAA_CLASS Solution
@@ -19,16 +15,16 @@ typedef long long ll;
 typedef long double ld;
 #define first x
 #define second y
-const int MEMORY_LIMIT_MB = 200;
-const int TIME_LIMIT_S = 1;
-const char input_filename[] =
+const size_t _ML = 200;
+const int _TL = 1;
+const char _inpf[] =
 #if defined(ANDREIKKAA)
 "input.txt"
 #else
 ""
 #endif
 ;
-const char output_filename[] =
+const char _outf[] =
 #if defined(ANDREIKKAA)
 ""
 #else
@@ -36,59 +32,22 @@ const char output_filename[] =
 #endif
 ;
 
-/* ______ INCLUDES ______ */
-#include <cassert>
-#include <ciso646>
-#include <climits>
-#include <cmath>
-#include <cstdint>
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <ctime>
-#include <array>
-#include <bitset>
-#include <deque>
-#include <forward_list>
-#include <list>
-#include <map>
-#include <queue>
-#include <set>
-#include <stack>
-#include <unordered_map>
-#include <unordered_set>
-#include <vector>
-#include <algorithm>
-#include <complex>
-#include <functional>
-#include <initializer_list>
-#include <iterator>
-#include <limits>
-#include <locale>
-#include <numeric>
-#include <regex>
-#include <string>
-#include <utility>
-#include <fstream>
-#include <iostream>
-#include <sstream>
-#include <iomanip>
+#include "bits/stdc++.h"
 using namespace std;
 
-/* _____ ALLOCATION _____ */
-
 #if defined(ANDREIKKAA_ALLOCATOR)
-char alloc_memory[MEMORY_LIMIT_MB * 1000 * 1000];
-size_t alloc_pointer = 0;
-inline void* operator new(size_t x) { alloc_pointer += x; return alloc_memory + alloc_pointer - x; }
+char _mem[_ML * 1024 * 1024];
+size_t _ptr = 0;
+inline void* operator new(size_t _x) { _ptr += _x; return _mem + _ptr - _x; }
 inline void operator delete(void*) { }
 #endif
 
-/*____ I-O OPERATORS ____*/
-template<typename T, typename U> inline ostream &operator<<(ostream &out, const pair<T, U> &p) { out << p.first << ' ' << p.second; return out; }
-template<typename T, typename U> inline istream &operator >> (istream &in, pair<T, U> &p) { in >> p.first >> p.second; return in; }
-template<typename T> inline ostream &operator<<(ostream &out, const vector<T> &v) { if (v.empty()) return out; out << v.front(); for (auto it = ++v.begin(); it != v.end(); ++it) out << ' ' << *it; return out; }
-template<typename T> inline istream &operator >> (istream &in, vector<T> &v) { for (auto &i : v) in >> i; return in; }
+template<typename T, typename U> inline ostream &operator<<(ostream &_out, const pair<T, U> &_p) { _out << _p.first << ' ' << _p.second; return _out; }
+template<typename T, typename U> inline istream &operator >> (istream &_in, pair<T, U> &_p) { _in >> _p.first >> _p.second; return _in; }
+template<typename T> inline ostream &operator<<(ostream &_out, const vector<T> &_v) { if (_v.empty()) return _out; _out << _v.front(); for (auto _it = ++_v.begin(); _it != _v.end(); ++_it) _out << ' ' << *_it; return _out; }
+template<typename T> inline istream &operator>> (istream &_in, vector<T> &_v) { for (auto &_i : _v) _in >> _i; return _in; }
+template<typename T> inline ostream &operator<<(ostream &_out, const set<T> &_s) { if (_s.empty()) return _out; _out << *_s.begin(); for (auto _it = ++_s.begin(); _it != _s.end(); ++_it) _out << ' ' << *_it; return _out; }
+template<typename T, typename U> inline ostream &operator<<(ostream &_out, const map<T, U> &_m) { if (_m.empty()) return _out; _out << _m.begin()->first << ": " << _m.begin()->second; for (auto _it = ++_m.begin(); _it != _m.end(); ++_it) _out << ", " << _it->first << ": " << _it->second; return _out; }
 
 /* ________ CODE ________ */
 
@@ -97,35 +56,30 @@ inline ANDREIKKAA_RETURN_TYPE mainFunction(ANDREIKKAA_PARAMETERS)
 
 }
 
-class ANDREIKKAA_CLASS
-{
-public:
-	ANDREIKKAA_RETURN_TYPE ANDREIKKAA_METHOD(ANDREIKKAA_PARAMETERS)
-	{
-		if (input_filename[0] != '\0')
-			freopen(input_filename, "r", stdin);
-		if (output_filename[0] != '\0')
-			freopen(output_filename, "w", stdout);
-		cin.tie(nullptr);
-		ios_base::sync_with_stdio(false);
-		//cout << setprecision(20);
-		//cout << fixed;
-		return mainFunction(ANDREIKKAA_CALL);
-	}
-#if defined(ANDREIKKAA)
-	inline ANDREIKKAA_CLASS() { _start = clock(); }
-	inline ~ANDREIKKAA_CLASS() { cerr << "Time: " << (clock() - _start) / (ld)CLOCKS_PER_SEC << endl; }
-private:
-	time_t _start;
-#endif
-};
+/* ________ CODE ________ */
 
 #if defined(ANDREIKKAA) || !defined(ANDREIKKAA_TOPCODER)
 int main()
 {
-	auto _s = new ANDREIKKAA_CLASS; _s->ANDREIKKAA_METHOD(ANDREIKKAA_CALL); delete _s;
 #if defined(ANDREIKKAA)
+	time_t _start = clock();
+#endif
+
+	if (sizeof(_inpf) > 1)
+		freopen(_inpf, "r", stdin);
+	if (sizeof(_outf) > 1)
+		freopen(_outf, "w", stdout);
+	cin.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+	//cout << setprecision(20);
+	//cout << fixed;
+	mainFunction(ANDREIKKAA_CALL);
+	
+#if defined(ANDREIKKAA)
+	cout << "Time: " << (clock() - _start) / (ld)CLOCKS_PER_SEC << endl;
 	while (true);
 #endif
 }
+#else
+class ANDREIKKAA_CLASS { public: ANDREIKKAA_RETURN_TYPE ANDREIKKAA_METHOD(ANDREIKKAA_PARAMETERS) { return mainFunction(ANDREIKKAA_CALL); } };
 #endif
