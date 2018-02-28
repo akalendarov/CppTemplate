eb7c1f793fab835ebd5e6d36146d64ab541b9429
Andrei Kalendarov <andreykalendarov@gmail.com>
2018-02-28 04:29:40 +0300

    Split declaration and definition of IO operators.

--- main.cpp
+++ main.cpp
@@ -9,23 +9,23 @@ const int _ML = 228;
 
 const char _inpf[] =
 #if defined(ANDREIKKAA)
-"input.txt"
+    "input.txt"
 #else
-""
+    ""
 #endif
 ;
 const char _outf[] =
 #if defined(ANDREIKKAA)
-""
+    ""
 #else
-""
+    ""
 #endif
 ;
 
 #if defined(ANDREIKKAA)
 #undef NDEBUG
 #else
-#pragma GCC optimize ("O3")
+#pragma GCC optimize("O3,no-stack-protector")
 #endif
 #include <bits/stdc++.h>
 #if !defined(ANDREIKKAA)
@@ -48,16 +48,16 @@ size_t _ptr = 0;
 inline void* operator new (size_t _x) { _ptr += _x; assert(_ptr < _ML * 1024 * 1024); return _mem + _ptr - _x; }
 inline void operator delete (void*) { }
 #endif
-template<typename T, typename U> inline ostream &operator<< (ostream &_out, const pair<T, U> &_p) { _out << _p.first << ' ' << _p.second; return _out; }
-template<typename T, typename U> inline istream &operator>> (istream &_in, pair<T, U> &_p) { _in >> _p.first >> _p.second; return _in; }
-template<typename T> inline ostream &operator<< (ostream &_out, const vector<T> &_v) { if (_v.empty()) { return _out; } _out << _v.front(); for (auto _it = ++_v.begin(); _it != _v.end(); ++_it) { _out << ' ' << *_it; } return _out; }
-template<typename T> inline istream &operator>> (istream &_in, vector<T> &_v) { for (auto &_i : _v) { _in >> _i; } return _in; }
-template<typename T> inline ostream &operator<< (ostream &_out, const set<T> &_s) { if (_s.empty()) { return _out; } _out << *_s.begin(); for (auto _it = ++_s.begin(); _it != _s.end(); ++_it) { _out << ' ' << *_it; } return _out; }
-template<typename T> inline ostream &operator<< (ostream &_out, const multiset<T> &_s) { if (_s.empty()) { return _out; } _out << *_s.begin(); for (auto _it = ++_s.begin(); _it != _s.end(); ++_it) { _out << ' ' << *_it; } return _out; }
-template<typename T> inline ostream &operator<< (ostream &_out, const unordered_set<T> &_s) { if (_s.empty()) { return _out; } _out << *_s.begin(); for (auto _it = ++_s.begin(); _it != _s.end(); ++_it) { _out << ' ' << *_it; } return _out; }
-template<typename T> inline ostream &operator<< (ostream &_out, const unordered_multiset<T> &_s) { if (_s.empty()) { return _out; } _out << *_s.begin(); for (auto _it = ++_s.begin(); _it != _s.end(); ++_it) { _out << ' ' << *_it; } return _out; }
-template<typename T, typename U> inline ostream &operator<< (ostream &_out, const map<T, U> &_m) { if (_m.empty()) { return _out; } _out << '(' << _m.begin()->first << ": " << _m.begin()->second << ')'; for (auto _it = ++_m.begin(); _it != _m.end(); ++_it) { _out << ", (" << _it->first << ": " << _it->second << ')'; } return _out; }
-template<typename T, typename U> inline ostream &operator<< (ostream &_out, const unordered_map<T, U> &_m) { if (_m.empty()) { return _out; } _out << '(' << _m.begin()->first << ": " << _m.begin()->second << ')'; for (auto _it = ++_m.begin(); _it != _m.end(); ++_it) { _out << ", (" << _it->first << ": " << _it->second << ')'; } return _out; }
+template<class T, class U> inline ostream &operator<< (ostream &, const pair<T, U> &);
+template<class T, class U> inline istream &operator>> (istream &, pair<T, U> &);
+template<class T> inline ostream &operator<< (ostream &, const vector<T> &);
+template<class T> inline istream &operator>> (istream &, vector<T> &);
+template<class T> inline ostream &operator<< (ostream &, const set<T> &);
+template<class T> inline ostream &operator<< (ostream &, const multiset<T> &);
+template<class T> inline ostream &operator<< (ostream &, const unordered_set<T> &);
+template<class T> inline ostream &operator<< (ostream &, const unordered_multiset<T> &);
+template<class T, class U> inline ostream &operator<< (ostream &, const map<T, U> &);
+template<class T, class U> inline ostream &operator<< (ostream &, const unordered_map<T, U> &);
 
 /* ________ CODE ________ */
 
@@ -85,4 +85,69 @@ int main()
 #if defined(ANDREIKKAA)
     cout << "Time: " << (clock() - _start) / (ld)CLOCKS_PER_SEC << endl;
 #endif
+}
+
+template<class T, class U> inline ostream &operator<< (ostream &_out, const pair<T, U> &_p)
+{
+    _out << _p.x << ' ' << _p.y;
+    return _out;
+}
+template<class T, class U> inline istream &operator>> (istream &_in, pair<T, U> &_p)
+{
+    _in >> _p.x >> _p.y;
+    return _in;
+}
+template<class T> inline ostream &operator<< (ostream &_out, const vector<T> &_v)
+{
+    if (_v.empty()) { return _out; }
+    _out << *_v.begin();
+    for (auto _it = ++_v.begin(); _it != _v.end(); ++_it) { _out << ' ' << *_it; }
+    return _out;
+}
+template<class T> inline istream &operator>> (istream &_in, vector<T> &_v)
+{
+    for (auto &_i : _v) { _in >> _i; }
+    return _in;
+}
+template<class T> inline ostream &operator<< (ostream &_out, const set<T> &_s)
+{
+    if (_s.empty()) { return _out; }
+    _out << *_s.begin();
+    for (auto _it = ++_s.begin(); _it != _s.end(); ++_it) { _out << ' ' << *_it; }
+    return _out;
+}
+template<class T> inline ostream &operator<< (ostream &_out, const multiset<T> &_s)
+{
+    if (_s.empty()) { return _out; }
+    _out << *_s.begin();
+    for (auto _it = ++_s.begin(); _it != _s.end(); ++_it) { _out << ' ' << *_it; }
+    return _out;
+}
+template<class T> inline ostream &operator<< (ostream &_out, const unordered_set<T> &_s)
+{
+    if (_s.empty()) { return _out; }
+    _out << *_s.begin();
+    for (auto _it = ++_s.begin(); _it != _s.end(); ++_it) { _out << ' ' << *_it; }
+    return _out;
+}
+template<class T> inline ostream &operator<< (ostream &_out, const unordered_multiset<T> &_s)
+{
+    if (_s.empty()) { return _out; }
+    _out << *_s.begin();
+    for (auto _it = ++_s.begin(); _it != _s.end(); ++_it) { _out << ' ' << *_it; }
+    return _out;
+}
+template<class T, class U> inline ostream &operator<< (ostream &_out, const map<T, U> &_m)
+{
+    if (_m.empty()) { return _out; }
+    _out << '(' << _m.begin()->x << ": " << _m.begin()->y << ')';
+    for (auto _it = ++_m.begin(); _it != _m.end(); ++_it) { _out << ", (" << _it->x << ": " << _it->y << ')'; }
+    return _out;
+}
+template<class T, class U> inline ostream &operator<< (ostream &_out, const unordered_map<T, U> &_m)
+{
+    if (_m.empty()) { return _out; }
+    _out << '(' << _m.begin()->x << ": " << _m.begin()->y << ')';
+    for (auto _it = ++_m.begin(); _it != _m.end(); ++_it) { _out << ", (" << _it->x << ": " << _it->y << ')'; }
+    return _out;
 }
