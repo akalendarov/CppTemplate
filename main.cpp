f55da06820c70f0ce9125116964831be3c4ab602
andreikkaa <andreykalendarov@gmail.com>
2019-07-30 17:14:50 +0300

    Optimized for clang

--- main.cpp
+++ main.cpp
@@ -27,7 +27,7 @@ const char _outf[] =
 #else
 #pragma GCC optimize("O3,no-stack-protector")
 #endif
-#include <bits/stdc++.h>
+#include "bits/stdc++.h"
 #if !defined(ANDREIKKAA)
 #define endl '\n'
 #endif
@@ -38,8 +38,8 @@ using namespace std;
 #define random_shuffle abcdefghijklmnopqrstuvwxyz
 #define all(x) (x).begin(), (x).end()
 #define sz(x) (int)(x).size()
-typedef long long ll;
-typedef double ld;
+using ll = long long;
+using ld = double;
 const ld PI = 3.14159265358979323846;
 mt19937 rd(228);
 #if defined(ANDREIKKAA_ALLOCATOR)
@@ -61,15 +61,13 @@ template<class T, class U> inline ostream &operator<< (ostream &, const unordere
 
 /* ________ CODE ________ */
 
-inline void _main_function()
-{
+inline void _main_function() {
 
 }
 
 /* ________ CODE ________ */
 
-int main()
-{
+int main() {
 #if defined(ANDREIKKAA)
     time_t _start = clock();
 #endif
@@ -87,65 +85,55 @@ int main()
 #endif
 }
 
-template<class T, class U> inline ostream &operator<< (ostream &_out, const pair<T, U> &_p)
-{
+template<class T, class U> inline ostream &operator<< (ostream &_out, const pair<T, U> &_p) {
     _out << _p.x << ' ' << _p.y;
     return _out;
 }
-template<class T, class U> inline istream &operator>> (istream &_in, pair<T, U> &_p)
-{
+template<class T, class U> inline istream &operator>> (istream &_in, pair<T, U> &_p) {
     _in >> _p.x >> _p.y;
     return _in;
 }
-template<class T> inline ostream &operator<< (ostream &_out, const vector<T> &_v)
-{
+template<class T> inline ostream &operator<< (ostream &_out, const vector<T> &_v) {
     if (_v.empty()) { return _out; }
     _out << *_v.begin();
     for (auto _it = ++_v.begin(); _it != _v.end(); ++_it) { _out << ' ' << *_it; }
     return _out;
 }
-template<class T> inline istream &operator>> (istream &_in, vector<T> &_v)
-{
+template<class T> inline istream &operator>> (istream &_in, vector<T> &_v) {
     for (auto &_i : _v) { _in >> _i; }
     return _in;
 }
-template<class T> inline ostream &operator<< (ostream &_out, const set<T> &_s)
-{
+template<class T> inline ostream &operator<< (ostream &_out, const set<T> &_s) {
     if (_s.empty()) { return _out; }
     _out << *_s.begin();
     for (auto _it = ++_s.begin(); _it != _s.end(); ++_it) { _out << ' ' << *_it; }
     return _out;
 }
-template<class T> inline ostream &operator<< (ostream &_out, const multiset<T> &_s)
-{
+template<class T> inline ostream &operator<< (ostream &_out, const multiset<T> &_s) {
     if (_s.empty()) { return _out; }
     _out << *_s.begin();
     for (auto _it = ++_s.begin(); _it != _s.end(); ++_it) { _out << ' ' << *_it; }
     return _out;
 }
-template<class T> inline ostream &operator<< (ostream &_out, const unordered_set<T> &_s)
-{
+template<class T> inline ostream &operator<< (ostream &_out, const unordered_set<T> &_s) {
     if (_s.empty()) { return _out; }
     _out << *_s.begin();
     for (auto _it = ++_s.begin(); _it != _s.end(); ++_it) { _out << ' ' << *_it; }
     return _out;
 }
-template<class T> inline ostream &operator<< (ostream &_out, const unordered_multiset<T> &_s)
-{
+template<class T> inline ostream &operator<< (ostream &_out, const unordered_multiset<T> &_s) {
     if (_s.empty()) { return _out; }
     _out << *_s.begin();
     for (auto _it = ++_s.begin(); _it != _s.end(); ++_it) { _out << ' ' << *_it; }
     return _out;
 }
-template<class T, class U> inline ostream &operator<< (ostream &_out, const map<T, U> &_m)
-{
+template<class T, class U> inline ostream &operator<< (ostream &_out, const map<T, U> &_m) {
     if (_m.empty()) { return _out; }
     _out << '(' << _m.begin()->x << ": " << _m.begin()->y << ')';
     for (auto _it = ++_m.begin(); _it != _m.end(); ++_it) { _out << ", (" << _it->x << ": " << _it->y << ')'; }
     return _out;
 }
-template<class T, class U> inline ostream &operator<< (ostream &_out, const unordered_map<T, U> &_m)
-{
+template<class T, class U> inline ostream &operator<< (ostream &_out, const unordered_map<T, U> &_m) {
     if (_m.empty()) { return _out; }
     _out << '(' << _m.begin()->x << ": " << _m.begin()->y << ')';
     for (auto _it = ++_m.begin(); _it != _m.end(); ++_it) { _out << ", (" << _it->x << ": " << _it->y << ')'; }
